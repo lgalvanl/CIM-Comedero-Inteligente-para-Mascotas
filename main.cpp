fe2b54f39bf008f7f2355c31330c3d141bea714b
lgalvanl <lgalvanl@fi.uba.ar>
2024-07-05 22:04:26 +0000

    main principal

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
+#include "pet_feeder_system.h"
+#include "arm_book_lib.h"
 #include "mbed.h"
-
-// main() runs in its own thread in the OS
 int main()
 {
+    UnbufferedSerial uartBle(PD_5, PD_6, 9600);
+    petfeederSystemInit();
     while (true) {
-
+        petfeederSystemUpdate();
     }
 }
 
