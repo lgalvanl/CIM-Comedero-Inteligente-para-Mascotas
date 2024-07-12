546dd79430734a64196d32bbe8897289eda8b65f
lgalvanl <lgalvanl@fi.uba.ar>
2024-07-12 20:08:21 +0000

    .

--- main.cpp
+++ main.cpp
@@ -1,12 +1,11 @@
-#include "pet_feeder_system.h"
-#include "arm_book_lib.h"
-#include "mbed.h"
-int main()
-{
-    UnbufferedSerial uartBle(PD_5, PD_6, 9600);
-    petfeederSystemInit();
+#include "system.h"
+//#include "arm_book_lib.h"
+//#include "mbed.h"
+
+int main(){
+    SystemInit();
     while (true) {
-        petfeederSystemUpdate();
+        SystemUpdate();
     }
 }
 
