12eef6548e1374b9e96d3f7bba941a3aa67ec7fd
lgalvanl <lgalvanl@fi.uba.ar>
2024-07-26 16:07:38 +0000

    Se modifica DIAS_BAT

--- modules/timer/timer.h
+++ modules/timer/timer.h
@@ -7,7 +7,7 @@
 //=====[Declaration of public defines]=========================================
 #define SEGS_DIA 86400
 #define SEGS_DEMO 120
-#define DIAS_BAT 5
+#define DIAS_BAT 2
 //=====[Declaration of public data types]======================================
 
 static ds3231_calendar_t UltimaRecargaBat;
