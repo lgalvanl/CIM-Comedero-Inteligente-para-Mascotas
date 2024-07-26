d24ecbc304bc089ce2e20bb7ce28d26521b5056c
lgalvanl <lgalvanl@fi.uba.ar>
2024-07-26 14:30:10 +0000

    Version final

--- modules/serial_com/serial_com.cpp
+++ modules/serial_com/serial_com.cpp
@@ -31,37 +31,29 @@ bool DataRecibida = false;
 
 //=====[Implementations of public functions]===================================
 void SerialComUpdate(){
-    //DataRecibida = false;
     char receivedChar = bleComCharRead();
     if( receivedChar != '\0' ) {
         switch (receivedChar) {
         case 'D': 
             csv_a_cadena(cadena);
-            printf("se leyo: %s \n",cadena);
             inicializarArregloMascotas(arreglo_mascotas,cadena);
             break;
         case 'm': 
-            printf("App pide la info recolectada\n");
             mandar_data();
             break;
         case 'r': 
-            printf("App pide valor de uid\n");
             bleComStringWrite(uid);
-            printf("se leyo: %s \n",uid);
             break;    
         case 'l': 
-            printf("Ya se envio toda la info!\n");
             DataRecibida = true;
             break;
         case 'a': 
-            printf("La app pregunta si hay alguna alerta a recibir\n");
             if(HayAlerta){
                 bleComStringWrite(nombre_alerta);
                 HayAlerta = false;
             }
             break;
         default: 
-            printf("Error al leer\n"); 
             break;
     } 
     }
